f692f2f1c3cf6d2171724db48bfeca5014379803
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-11-01 08:50:31 +0530

    Added courses service

--- app/courses.component.js
+++ app/courses.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './course.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,23 +10,31 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, course_service_1;
     var CoursesComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (course_service_1_1) {
+                course_service_1 = course_service_1_1;
             }],
         execute: function() {
             CoursesComponent = (function () {
-                function CoursesComponent() {
+                function CoursesComponent(courseService) {
+                    this.titleCourses = "The title of course page.";
+                    this.titleAuthors = "The title of author page";
+                    this.authors = ['a1', 'a2', 'a3'];
+                    this.courses = courseService.getCourses();
                 }
                 CoursesComponent = __decorate([
                     core_1.Component({
                         selector: 'courses',
-                        template: "\n        <h2>Courses</h2>\n    "
+                        template: "\n        <h2>Courses</h2>\n        {{titleCourses}}\n        <ul>\n          <li *ngFor=\"#course of courses\">\n            {{course}}\n          </li>\n        </ul>\n\n        <h2>Authors</h2>\n        {{titleAuthors}}\n        <ul>\n          <li *ngFor=\"#author of authors\">\n            {{author}}\n          </li>\n        </ul>\n    ",
+                        providers: [course_service_1.CourseService]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [course_service_1.CourseService])
                 ], CoursesComponent);
                 return CoursesComponent;
             }());
--- app/courses.component.ts
+++ app/courses.component.ts
@@ -1,11 +1,34 @@
 import {Component} from 'angular2/core';
+import {CourseService} from './course.service';
 
 @Component({
   selector: 'courses',
   template: `
         <h2>Courses</h2>
-    `
+        {{titleCourses}}
+        <ul>
+          <li *ngFor="#course of courses">
+            {{course}}
+          </li>
+        </ul>
+
+        <h2>Authors</h2>
+        {{titleAuthors}}
+        <ul>
+          <li *ngFor="#author of authors">
+            {{author}}
+          </li>
+        </ul>
+    `,
+    providers: [CourseService]
 })
 export class CoursesComponent {
+  titleCourses = "The title of course page."; 
+  titleAuthors = "The title of author page";
+  courses;
+  authors = ['a1', 'a2', 'a3']
 
+  constructor(courseService: CourseService) {
+    this.courses = courseService.getCourses()
+  }
 }
